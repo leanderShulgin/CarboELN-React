69e63f0efa2095f04433fc0146c21c6284c74a3c
Lean <leandrohornos@gmail.com>
2020-07-23 00:52:08 -0300

    cambio Dashboard de clase a  arrowFunction

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,51 +6,50 @@ import DashboardProyectsTable from "./DashboardProjectsTable";
 import DashboardLinkBox from "./DashboardLinkBox";
 import DashboardUserBox from "./DashboardUserBox";
 
-class Dashboard extends React.Component {
-  render() {
-    return (
-      <div className="container-fluid main-container" id="main-container">
-        <header>
-          <div className="row">
-            <div className="col-lg-1"></div>
-            <div className="col-lg-7">
-              <div className="dashboard-header-box start-box">
-                <h1 id="titulo-dashboard">Carbono ELN</h1>
-                <h3>Inicio</h3>
-                <p className="texto-dashboard">
-                  Desde aquí podés acceder a todas las funciones del cuaderno de
-                  laboratorio electrónico
-                </p>
-              </div>
+
+const Dashboard = () => {
+  return (
+    <div className="container-fluid main-container" id="main-container">
+      <header>
+        <div className="row">
+          <div className="col-lg-1"></div>
+          <div className="col-lg-7">
+            <div className="dashboard-header-box start-box">
+              <h1 id="titulo-dashboard">Carbono ELN</h1>
+              <h3>Inicio</h3>
+              <p className="texto-dashboard">
+                Desde aquí podés acceder a todas las funciones del cuaderno de
+                laboratorio electrónico
+              </p>
             </div>
-            <div className="col-lg-3">
-              <DashboardUserBox />
-            </div>
-            <div className="col-lg-1"></div>
           </div>
-        </header>
-        <main>
-          <div className="row">
-            <div className="col-lg-1"></div>
-            <div className="col-lg-7">
-              <DashboardProyectsTable />
-            </div>
-            <div className="col-lg-3" id="box-nuevo-reporte">
-              <DashboardLinkBox
-                headerTitle="Nuevo Reporte"
-                bodyText="Haz click aquí para comenzar un nuevo reporte"
-              />
-              <DashboardLinkBox
-                headerTitle="Nuevo Proyecto"
-                bodyText="Haz click aquí para comenzar un nuevo proyecto"
-              />
-            </div>
-            <div className="col-lg-1"></div>
+          <div className="col-lg-3">
+            <DashboardUserBox />
           </div>
-        </main>
-      </div>
-    );
-  }
-}
+          <div className="col-lg-1"></div>
+        </div>
+      </header>
+      <main>
+        <div className="row">
+          <div className="col-lg-1"></div>
+          <div className="col-lg-7">
+            <DashboardProyectsTable />
+          </div>
+          <div className="col-lg-3" id="box-nuevo-reporte">
+            <DashboardLinkBox
+              headerTitle="Nuevo Reporte"
+              bodyText="Haz click aquí para comenzar un nuevo reporte"
+            />
+            <DashboardLinkBox
+              headerTitle="Nuevo Proyecto"
+              bodyText="Haz click aquí para comenzar un nuevo proyecto"
+            />
+          </div>
+          <div className="col-lg-1"></div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default Dashboard;
