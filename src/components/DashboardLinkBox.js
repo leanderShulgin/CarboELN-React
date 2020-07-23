d030387c7fc26c912e7b31ec0752e54639b361a8
Lean <leandrohornos@gmail.com>
2020-07-23 00:59:46 -0300

    paso DashboardLinkBox a functional component

--- src/components/DashboardLinkBox.js
+++ src/components/DashboardLinkBox.js
@@ -2,16 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./DashboardLinkBox.css";
 
-class DashboardLinkBox extends React.Component {
-  render() {
-    return (
-      <div className="start-box dashboard-link-box">
-        <h4>{this.props.headerTitle}</h4>
-        <hr />
-        <a href="reporte-editar.html">{this.props.bodyText}</a>
-      </div>
-    );
-  }
-}
+const DashboardLinkBox = (props) => {
+  return (
+    <div className="start-box dashboard-link-box">
+      <h4>{props.headerTitle}</h4>
+      <hr />
+      <a href="reporte-editar.html">{props.bodyText}</a>
+    </div>
+  );
+};
 
 export default DashboardLinkBox;
