d5655f1b0f267c1cf5da4642da2859ff45f98a22
Lean <leandrohornos@gmail.com>
2020-07-28 23:29:05 -0300

    comento imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register";
-import ProjectViewer from "./components/ProjectViewer/ProjectViewer";
-import ExperimentEditor from "./components/ExperimentEditor/ExperimentEditor";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import Login from "./components/Login.jsx";
+// import Register from "./components/Register";
+// import ProjectViewer from "./components/ProjectViewer/ProjectViewer";
+// import ExperimentEditor from "./components/ExperimentEditor/ExperimentEditor";
 import Welcome from "./components/Welcome.jsx";
 
 class App extends React.Component {
