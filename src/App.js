36160c82cd8aacc553c7707f768d2df939feaf43
Lean <leandrohornos@gmail.com>
2020-07-30 18:07:51 -0300

    Completo el switch en App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
-// import Login from "./components/Login.jsx";
-// import Register from "./components/Register";
-// import ProjectViewer from "./components/ProjectViewer/ProjectViewer";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register";
+import ProjectViewer from "./components/ProjectViewer/ProjectViewer";
 import ExperimentEditor from "./components/ExperimentEditor/ExperimentEditor";
 import Welcome from "./components/Welcome.jsx";
 
 const App = () => {
   const [page, setPage] = useState("");
-  // return <Dashboard />;
-  // return <Login />;
-  // return <Welcome />;
-  // return <Register />;
+
   switch (page) {
     case "dashboard":
       return <Dashboard />;
-      break;
+    case "login":
+      return <Login />;
+    case "register":
+      return <Register />;
+    case "experiment-editor":
+      return <ExperimentEditor />;
+    case "project-viewer":
+      return <ProjectViewer />;
     default:
-      return <Welcome setPage={setPage}/>;
+      return <Welcome setPage={setPage} />;
   }
   // return <ExperimentEditor />;
   // return <ProjectViewer />;
